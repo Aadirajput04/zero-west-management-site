823a064a9af3f2106c79eed7ac7d998d362c1463
aaditya singh <aadityasingh396@gmail.com>
2023-07-17 15:00:49 +0530

    header created succesfully

---
+++ header.php
@@ -0,0 +1,40 @@
+<header>
+
+    <div class="logo">
+        <img src="./assets/img/main-logo.png" alt="">
+    </div>
+    <div class="navbar-box">
+        <div class="social-line">
+            <div class="head-left-contact">
+                <a href=""><img src="./assets/img/call.svg" alt="">+91 99260 98541</a>
+                <a href=""><img src="./assets/img/mail.svg" alt="">Getinfo@gmail.com</a>
+            </div>
+
+            <div class="right-social-icons">
+                <a href=""><img src="./assets/img/Facebook - Negative.svg" alt=""></a>
+                <a href=""><img src="./assets/img/Twitter - Negative.svg" alt=""></a>
+                <a href=""><img src="./assets/img/Group.svg" alt=""></a>
+
+
+
+            </div>
+        </div>
+        <div class="navlist">
+            <ul>
+                <li> <a href="">home</a> </li>
+                <li> <a href="">about us</a> </li>
+                <li> <a href="">product & services</a> </li>
+                <li> <a href="">contact us</a> </li>
+                <li> <a href=""><img src="./assets/img/search-icon.svg" alt=""></a> </li>
+            </ul>
+
+
+        </div>
+
+
+    </div>
+
+
+
+
+</header>
